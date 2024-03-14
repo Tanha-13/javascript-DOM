e9a3f9ea9d99b12f6e2467324d797149bd2bd626
Tanha <nusrattanha897@gmail.com>
2024-03-14 22:51:16 +0600

    basic dom diagram

---
+++ notes.js
@@ -0,0 +1,7 @@
+console.log(window)
+//output: it will display/print the window object properties.
+console.log(window.document)
+//output: will display/print the page content.
+//imp: window.document == document. both generate same output.
+console.dir(document)
+//output: will display the hidden properties too.
