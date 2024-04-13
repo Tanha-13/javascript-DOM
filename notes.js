efcb5169c4d09dd632e8f3599aa1ef53418263e8
Tanha <nusrattanha897@gmail.com>
2024-04-13 16:43:35 +0600

    textContent, innerText, innerHTML

--- notes.js
+++ notes.js
@@ -1,9 +1,22 @@
-console.log(window)
+//console.log(window)
 //output: it will display/print the window object properties.
-console.log(window.document)
+//console.log(window.document)
 //output: will display/print the page content.
 //imp: window.document == document. both generate same output.
-console.dir(document)
+//console.dir(document)
 //output: will display the hidden properties too.
-console.log(document.baseURI)
-console.log(document.links);
+//console.log(document.baseURI)
+//console.log(document.links);
+
+// DOM Selectors
+/**
+ * 01.selectors are methods followed by parenthesis. This method requires a parameter to understand which element to target.
+ * 02. class == className. behind the scene, in DOM, class is portrayed as className. 
+ * 03. setAttribute('class', 'class list using space'). If we set one class, it will override the existing class. so include the existing class while setting attribute.
+ * 04. //imp: document.getElementById('').textContent vd document.getElementById('').innerText : innerText provides the visible text/content whereas textContent returns hidden content.
+ * 
+ * 05. innerHTML: innerHTML provides the whole content including inner tags, styles.
+ */
+
+
+
