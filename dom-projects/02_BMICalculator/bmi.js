912c178f37f6d720f215b95bb2d89720055c21ae
Tanha <nusrattanha897@gmail.com>
2024-04-24 00:38:04 +0600

    updated

--- dom-projects/02_BMICalculator/bmi.js
+++ dom-projects/02_BMICalculator/bmi.js
@@ -5,7 +5,6 @@ form.addEventListener('submit', (event) => {
     const height = parseFloat(document.querySelector('#height').value);
     const weight = parseFloat(document.querySelector('#weight').value);
     const result = document.querySelector('.result');
-    console.log(result);
 
     if(height === "" || height < 0 || isNaN(height) ){
         alert('Please provide a valid height');
@@ -27,13 +26,15 @@ form.addEventListener('submit', (event) => {
         <p style="color:green;">You are in normal range</p>
         `;
     }
-    else{
+    else if(bmi > 24.9){
         result.innerHTML = `
         <span>${bmi}</span>
         <p style="color:red;">You are over weight</p>
         `;
     }
+    else{
+        result.innerHTML = '';
+    }
     document.querySelector('#height').value = '';
     document.querySelector('#weight').value = '';
-
 })
