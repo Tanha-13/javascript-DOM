9502933c51ad745efc557ac834b457aef83867c9
Tanha <nusrattanha897@gmail.com>
2024-04-28 23:44:59 +0600

    guess number validation and checked

--- dom-projects/04_GuessNumber/guessNumber.js
+++ dom-projects/04_GuessNumber/guessNumber.js
@@ -12,9 +12,11 @@ const guessField = document.querySelector('#guessField');
 const submit = document.querySelector("#submitButton");
 const preGuess = document.querySelector(".guesses");
 const remaining = document.querySelector(".lastResult");
+const displayResult = document.querySelector(".result");
 
 //generating the random number
 const randomNumber = Math.floor(Math.random() * 100 + 1);
+console.log(randomNumber);
 
 //collecting the guess number from the user input using click event in submit button
 submit.addEventListener('click', (e) => {
@@ -24,6 +26,26 @@ submit.addEventListener('click', (e) => {
 })
 
 function validateGuess(guess){
+    if(isNaN(guess)){
+        return alert('Please provide a valid number');
+    }
+    else if(guess < 1 || guess > 100){
+        return alert('Please provide a number between 1 and 100');
+    }
+
+    checkGuess(guess);
+
     
-    
+}
+
+function checkGuess(guess){
+    if(guess === randomNumber){
+        return displayResult.innerHTML = `<h2>You guessed it right</h2>`;
+    }
+    else if(guess < randomNumber){
+        return displayResult.innerHTML = `<h2>Sorry, Your guess is low. Try Again</h2>`;
+    }
+    else if(guess > randomNumber){
+        return displayResult.innerHTML = `<h2>Sorry, Your guess is high. Try Again</h2>`;
+    }
 }
