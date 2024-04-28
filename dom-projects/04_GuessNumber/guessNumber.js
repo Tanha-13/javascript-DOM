1254c625a050d1597c77dd13aa1db7e49a9dc148
Tanha <nusrattanha897@gmail.com>
2024-04-28 23:13:55 +0600

    guess number project overview

---
+++ dom-projects/04_GuessNumber/guessNumber.js
@@ -0,0 +1,15 @@
+//imp: Project overview
+/**
+ * 01. validation
+ * 02. check the guess number will match the random number or not.
+ * 03. display previous and remaining guess
+ * 04. display message
+ * 05. end the game
+ * 06. start the message
+ */
+
+const guessField = document.querySelector('#guessField');
+const submit = document.querySelector("#submitButton");
+const preGuess = document.querySelector(".guesses");
+const remaining = document.querySelector(".lastResult");
+
